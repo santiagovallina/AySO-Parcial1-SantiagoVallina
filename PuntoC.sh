e10ddd2a06cbcc3bd719cd6dfe59991a70605145
santiagovallina <santiagovallina00@gmail.com>
2024-10-06 21:54:21 +0000

    punto C

--- PuntoC.sh
+++ PuntoC.sh
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 
+=======
+sudo useradd developer
+sudo groupadd grupoDevops
+sudo usermod -aG grupoDevops developer
+sudo useradd tester
+sudo groupadd grupodiseño
+sudo usermod -aG grupodiseño tester
+sudo useradd devops
+sudo groupadd grupoDeveloper
+sudo usermod -aG grupoDeveloper devops
+sudo useradd diseñador
+>>>>>>> d065d95 (punto C)
