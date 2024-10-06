a060a3d0998db3e7ea0848294e4b746f035ab426
santiagovallina <santiagovallina00@gmail.com>
2024-10-06 23:21:34 +0000

    corrección punto C

--- PuntoC.sh
+++ PuntoC.sh
@@ -3,18 +3,21 @@
 sudo useradd developer
 sudo groupadd grupoDevops
 sudo usermod -aG grupoDevops developer
+
 sudo useradd tester
 sudo groupadd grupodiseño
 sudo usermod -aG grupodiseño tester
+
 sudo useradd devops
 sudo groupadd grupoDeveloper
 sudo usermod -aG grupoDeveloper devops
+
 sudo useradd diseñador
 
-sudo chmod 750 /home/Examenes-UTN/alumno_1
-sudo chmod 760 /home/Examenes-UTN/alumno_2
-sudo chmod 700 /home/Examenes-UTN/alumno_3
-sudo chmod 775 /home/Examenes-UTN/profesores
+sudo chmod -R 750 Examenes-UTN/alumno_1
+sudo chmod -R 760 Examenes-UTN/alumno_2
+sudo chmod -R 700 Examenes-UTN/alumno_3
+sudo chmod -R 775 Examenes-UTN/profesores
 
 
 whoami > validar.txt
