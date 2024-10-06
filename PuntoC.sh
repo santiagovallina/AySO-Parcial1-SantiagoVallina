b809ff7d4d2578f708a190f16d148648c2a6bca4
santiagovallina <santiagovallina00@gmail.com>
2024-10-06 22:42:06 +0000

    punto c completo

--- PuntoC.sh
+++ PuntoC.sh
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-
 =======
 sudo useradd developer
 sudo groupadd grupoDevops
@@ -11,4 +10,15 @@ sudo useradd devops
 sudo groupadd grupoDeveloper
 sudo usermod -aG grupoDeveloper devops
 sudo useradd diseñador
+
+sudo chmod 750 /home/Examenes-UTN/alumno_1
+sudo chmod 760 /home/Examenes-UTN/alumno_2
+sudo chmod 700 /home/Examenes-UTN/alumno_3
+sudo chmod 775 /home/Examenes-UTN/profesores
+
+
+whoami > validar.txt
 >>>>>>> d065d95 (punto C)
+
+
+
